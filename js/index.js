e4b6fc1eb41c469b812d37cb9e48a8994e27c68a
Rachel Wyatt <rw126@evansville.edu>
2019-04-17 18:51:57 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,13 +1,26 @@
-// use this to copy code snippets or use your browser's console
-function greet() {
-    console.log(`my name is ${this.name}, hi!`);
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
 }
  
-greet(); // my name is , hi!
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
  
-let person = {
-    name: 'Bob',
-    greet: greet
-};
+    matchInterests(event) {
+        return event.keywords.some(
+            function(word) {
+                return this.interests.includes(word);
+            }.bind(this) // added to the and of the callback function
+        );
+    }
+}
  
-person.greet(); 
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+ 
+billy.matchInterests(freeMusic);
